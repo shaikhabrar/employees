2c1450124795a2baf0e2ebebc602f2266e2c1e00
abrar <shaikhabrar637@gmail.com>
2023-07-15 20:00:04 +0530

    added integration for user data

--- login/src/App.js
+++ login/src/App.js
@@ -1,30 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
-import Login from './login';
-import FileUploader from './fileUploader';
-import Signup from './signup';
-import './FileUploader.css';
-import Popup from './popup';
-import Birthdays from './birthdays';
-import HomePage from './HomePage';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
 
-// ReactDOM.render(<HomePage />, document.getElementById('root'));
+import Login from "./login";
+import FileUploader from "./fileUploader";
+import Signup from "./signup";
+import "./FileUploader.css";
+import Popup from "./popup";
+import Birthdays from "./birthdays";
+import HomePage from "./HomePage";
+import { toast } from 'react-toastify';
 
 
 const App = () => {
-  return (<>
-    <Router>
-    <Routes>
-      <Route exact path="/" element={<Login/>} />
-      <Route path="/signUp" element={<Signup/>} />
-      <Route path="/Fileuploader" element={<FileUploader/>} />
-      <Route path="/popup" element={<Popup/>} />
-      <Route path="/birthdays" element={<Birthdays/>} />
-      <Route path="/homePage" element={<HomePage/>} />
+  const isLoggedIn = sessionStorage.getItem("loggedinUserData")?.length > 0;
 
-    </Routes>
-  </Router>
-  </>
+  const PrivateRoute = ({ element: Component, ...props }) => {
+    return isLoggedIn ? <Component {...props} /> : <Navigate to="/" />;
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signUp" element={<Signup />} />
+        <Route
+          path="/Fileuploader"
+          element={<PrivateRoute element={FileUploader} />}
+        />
+        <Route path="/popup" element={<PrivateRoute element={Popup} />} />
+        <Route
+          path="/birthdays"
+          element={<PrivateRoute element={Birthdays} />}
+        />
+        <Route path="/homePage" element={<PrivateRoute element={HomePage} />} />
+      </Routes>
+    </Router>
   );
 };
 
